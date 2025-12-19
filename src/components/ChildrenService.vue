ed9804e8e5587e30a0cecaf4801eaac0a0714e85
schier-jonasz <schier.jonasz@gmail.com>
2025-12-19 14:42:15 +0100

    update: changed picture in children service section

--- src/components/ChildrenService.vue
+++ src/components/ChildrenService.vue
@@ -2,7 +2,7 @@
   <section id="children-service" class="flex items-center justify-center">
     <div class="grid grid-cols-[485px_1fr] h-auto max-lg:grid-cols-1 max-lg:w-full w-full max-w-[1200px]">
       <div class="h-full max-lg:order-1 max-lg:h-[200px]">
-        <img src="@/assets/img/children.png" alt="Children picture" class="w-full h-full object-cover rounded-s-4xl max-lg:rounded-t-4xl max-lg:rounded-b-none"/>
+        <img src="@/assets/img/pencils.jpg" alt="Pencils picture" class="w-full h-full object-cover rounded-s-4xl max-lg:rounded-t-4xl max-lg:rounded-b-none"/>
       </div>
       <div class="bg-secondary py-[65px] pl-[64px] pr-12 flex flex-col gap-6 rounded-e-4xl max-lg:py-8 max-lg:px-6 max-lg:rounded-b-4xl max-lg:rounded-t-none max-lg:order-2 max-lg:items-center">
         <h3 class="text-5xl font-bold max-lg:text-2xl max-lg:text-center">"Pozwólcie dzieciom przychodzić do mnie..."</h3>
